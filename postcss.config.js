a35286bdd264dccb9717a2d7b3aafe7142ba114b
Victor Saa <vis97c@hotmail.com>
2023-05-27 18:50:17 -0500

    refactor: use cascade layers
    BREAKING CHANGE: flxSelf as union alongside flxAlignSelf & flxJustifySelf as independedt modifiers

--- postcss.config.js
+++ postcss.config.js
@@ -2,38 +2,25 @@ const nano = {
 	preset: [
 		"advanced",
 		{
-			discardComments: {
-				removeAll: true,
-			},
-			calc: false,
+			discardComments: { removeAll: true },
 			zindex: false,
-			autoprefixer: false,
 		},
 	],
 };
 
-const sortCSSmq = require("sort-css-media-queries")({
-	unitlessMqAlwaysFirst: true,
-});
+const createSort = require("sort-css-media-queries/lib/create-sort");
+const sortCSSmq = createSort({ unitlessMqAlwaysFirst: true });
 
 module.exports = {
-	// Add plugin names as key and arguments as value
-	// Install them before as dependencies with npm or yarn
 	map: false,
 	plugins: [
 		// Optimizations
-		require("node-css-mqpacker")({ sort: false }),
 		require("cssnano")(nano),
 		require("postcss-merge-selectors")({}),
-		require("postcss-precision")({}),
-		// optimize again
 		require("node-css-mqpacker")({
 			sort: sortCSSmq.desktopFirst,
 		}),
-		require("cssnano")(nano),
-		// More weight by polyfilling
-		require("autoprefixer")({}),
-		require("postcss-custom-properties")({}),
+		require("postcss-precision")({}),
 	],
 	env: "production",
 	preset: { stage: false },
