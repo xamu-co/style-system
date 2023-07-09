6c4a5d49ff11dbcfad72e5c613223d32660a5c5a
Victor Saa <vis97c@hotmail.com>
2023-07-09 17:19:21 -0500

    feat: text spacing
    fix: gaping none

--- postcss.config.js
+++ postcss.config.js
@@ -1,32 +1,26 @@
-const nano = {
-	preset: [
-		"advanced",
-		{
-			discardComments: { removeAll: true },
-			zindex: false,
-		},
-	],
-};
-
 module.exports = {
 	map: false,
 	plugins: [
 		// Optimizations
 		require("postcss-preset-env")({
 			stage: 4,
-			features: {
-				"nesting-rules": { noIsPseudoSelector: true },
-			},
+			features: { "nesting-rules": { noIsPseudoSelector: true } },
 			minimumVendorImplementations: 3,
 			autoprefixer: false,
 		}),
-		require("cssnano")(nano),
+		require("cssnano")({
+			preset: [
+				"advanced",
+				{
+					discardComments: { removeAll: true },
+					zindex: false,
+				},
+			],
+		}),
 		require("postcss-merge-selectors")({}),
 		require("postcss-sort-media-queries")({
 			sort: "desktop-first",
-			configuration: {
-				unitlessMqAlwaysFirst: true,
-			},
+			configuration: { unitlessMqAlwaysFirst: true },
 		}),
 		require("postcss-merge-at-rules")({ nest: true }),
 		require("postcss-precision")({}),
